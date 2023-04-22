44d5c5cf874e434d8313eb9add8c494a255031b9
Yogananda Rao Locherla <locherla@live.com>
2023-04-22 10:58:50 +0530

    Customer Matcher Example

--- src/concepts/matchers/matchers.test.js
+++ src/concepts/matchers/matchers.test.js
@@ -7,3 +7,28 @@ test('object assignment', () => {
     data['lname'] = 'L';
     expect(data).toEqual({ fname: 'Yoga', lname: 'L' });
 });
+
+// Customer matcher
+
+expect.extend({
+    toEqualCaseInsensitive(received, expected) {
+        const pass = JSON.stringify(received).toLowerCase() === JSON.stringify(expected).toLowerCase();
+        if (pass) {
+            return {
+                message: () => `expected ${received} not to equal ${expected} (case insensitive)`,
+                pass: true,
+            };
+        } else {
+            return {
+                message: () => `expected ${received} to equal ${expected} (case insensitive)`,
+                pass: false,
+            };
+        }
+    },
+});
+
+test('object comparision - Case sensitive', () => {
+    const data = { fname: 'Yoga' };
+    data['lname'] = 'L';
+    expect(data).toEqualCaseInsensitive({ fname: 'yoga', lname: 'l' });
+});
